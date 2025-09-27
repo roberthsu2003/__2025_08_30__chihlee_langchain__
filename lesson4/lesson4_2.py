aea256948a20ec0abc0da6b0ced3b8fc465e3c99
徐國堂 <roberthsu2003@gmail.com>
2025-09-27 09:16:29 +0800

    modify

--- lesson4/lesson4_2.py
+++ lesson4/lesson4_2.py
@@ -29,4 +29,4 @@ iface = gr.Interface(
 )
 
 if __name__ == "__main__":
-    iface.launch(server_name="0.0.0.0", server_port=7860)
+    iface.launch(server_name="0.0.0.0", server_port=7860,share=True)
